8ef29f185934a3d81eb0a34ea8b23dd11d9c88e8
bm78g <isntglutencakejustbread@gmail.com>
2026-09-03 14:43:09 -0500

    feat: draw annotation polygons

--- src/calibration.py
+++ src/calibration.py
@@ -1,6 +1,7 @@
 import cv2
 import sys
 import json
+import numpy as np
 
 img = cv2.imread('data/sample.jpg')
 
@@ -13,14 +14,18 @@ polygons = []
 def click_event(event, x, y, flags, param):
     global vertices, polygons
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img, center=(x, y), radius=10, color=(0, 0, 255), thickness=-1)
-        cv2.imshow("display", img)
+        cv2.circle(img, center=(x, y), radius=3, color=(0, 0, 255), thickness=-1)
         vertices.append([x, y])
         if len(vertices) == 4:
             polygons.append(vertices.copy())
             vertices = []
-            print("polygons", polygons)
-            print(len(polygons))
+
+            pts = np.array(polygons[len(polygons) - 1], np.int32)
+            print(pts)
+            pts = pts.reshape((-1, 1, 2))
+            print(pts)
+            cv2.polylines(img, [pts], True, (0, 255, 255))
+        cv2.imshow("display", img)
 
 cv2.imshow("display", img)
 cv2.setMouseCallback("display", click_event)
